a16dc49ed72d300f0f66ff3671c394e8bc1e9eb7
alex <xyzuser@yandex.ru>
2019-08-12 17:08:07 +0300

    docker machine added

--- install-docker.sh
+++ install-docker.sh
@@ -20,3 +20,9 @@ sudo add-apt-repository \
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo setfacl -m user:$(whoami):rw /var/run/docker.sock
+
+
+curl -L https://github.com/docker/machine/releases/download/v0.16.1/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+chmod +x /tmp/docker-machine &&
+sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
